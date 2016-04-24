806bc4407d40a0b407c873fb04939273efbf4b53
Chris Sinclair <chris@quarterstaff.co.uk>
2016-04-24 11:42:10 +0100

    Rewrote to use classes

--- scrape_your_plate.py
+++ scrape_your_plate.py
@@ -12,147 +12,180 @@ import getpass
 import re
 import os
 
-def pp_login(username, password):
-    if not password:
-        password = getpass.getpass('Please enter the password for account {}: '.format(username))
+class pepperplate_recipe:
+    def __init__(self, id, html):
+        self.id = id
+        self.soup = BeautifulSoup(html)
 
-    print('Logging into pepperplate.com')
+    def get_id(self):
+        return self.id
 
-    url = 'https://www.pepperplate.com/login.aspx'
-    headers = {"User-Agent":"Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2062.120 Safari/537.36"}
+    def get_title(self):
+        return self.soup.find(id='cphMiddle_cphMain_lblTitle').get_text().strip()
 
-    s = requests.Session()
-    s.headers.update(headers)
-    r = s.get(url)
+    def get_new_body(self):
+        new_soup = BeautifulSoup('<html><head></head><body></body></html>')
 
-    soup = BeautifulSoup(r.content)
+        thumb = self.get_thumbnail()
+        if thumb:
+            hdr = new_soup.new_tag('img')
 
-    VIEWSTATE = soup.find(id='__VIEWSTATE')['value']
-    EVENTVALIDATION = soup.find(id='__EVENTVALIDATION')['value']
+            hdr['src'] = './img/{}'.format(self.id + '.jpg')
+            new_soup.body.append(hdr)
 
-    login_data={"__VIEWSTATE":VIEWSTATE,
-    "__EVENTVALIDATION":EVENTVALIDATION,
-    "__EVENTARGUMENT":'',
-    "__EVENTTARGET":'ctl00$cphMain$loginForm$ibSubmit',
-    "ctl00$cphMain$loginForm$tbEmail":username,
-    "ctl00$cphMain$loginForm$tbPassword":password,
-    "ctl00$cphMain$loginForm$cbRememberMe":'on'
-    }
-
-    r = s.post(url, data=login_data)
-    if r.url == 'http://www.pepperplate.com/recipes/default.aspx':
-        print('Login sucessful')
-    else:
-        print('Login failed. Please try again')
-        exit(1)
-
-    return s
-
-def pp_get_page(session, page):
-    print('Downloading page {} of recipes'.format(page+1))
-
-    url = 'http://www.pepperplate.com/recipes/default.aspx/GetPageOfResults'
-    parameters = json.dumps({'pageIndex':page,
-                             'pageSize':20,
-                             'sort':4,
-                             'tagIds': [],
-                             'favoritesOnly':0})
-
-    headers={'Referer':'http://www.pepperplate.com/recipes/default.aspx'
-                     ,'Content-Type': 'application/json'
-                     ,'X-Requested-With': 'XMLHttpRequest'
-                     ,'DNT':1
-                     ,'Accept': 'application/json, text/javascript, */*; q=0.01'
-                     ,'Accept-Language': 'en,de;q=0.7,en-US;q=0.3'
-                     ,'Accept-Encoding': 'gzip, deflate'}
-    r = session.request('POST', url, data=parameters, headers=headers)
-
-    soup = BeautifulSoup(r.json()['d'])
-    return soup
-
-def scrape_recipe_ids(soup):
-    ids = [re.findall(r'id=(\d+)', a['href'])[0] for d in soup.find_all('div',{'class':'item'}) for a in d.find_all('a')]
-    print('Found {} recipes'.format(len(ids)))
-    return ids
-
-def get_recipe(session, id, imgpath):
-    url = 'http://www.pepperplate.com/recipes/view.aspx?id={}'.format(id)
-    r = session.request('GET', url)
-    soup = BeautifulSoup(r.content)
-
-    title = soup.find(id='cphMiddle_cphMain_lblTitle').get_text().strip()
-    print('Downloaded "{}"'.format(title))
-
-    thumb = soup.find(id='cphMiddle_cphMain_imgRecipeThumb')
-    if thumb:
-        print('* Downloading thumbnail')
-        r = requests.get(thumb['src'])
-
-        m = re.search('recipes/(.+\.jpg)', thumb['src'])
-        with open(imgpath.format(m.group(1)),'wb') as img:
-            img.write(r.content)
-
-    return title, soup
-
-def format_recipe(old_soup):
-    new_soup = BeautifulSoup('<html><head></head><body></body></html>')
-
-    thumb = old_soup.find(id='cphMiddle_cphMain_imgRecipeThumb')
-    if thumb:
-        hdr = new_soup.new_tag('img')
-
-        m = re.search('recipes/(.+\.jpg)', thumb['src'])
-
-        hdr['src'] = './img/{}'.format(m.group(1))
+        #Title
+        title = self.get_title()
+        hdr = new_soup.new_tag('title')
+        hdr.append(title)
+        new_soup.head.append(hdr)
+    
+        hdr = new_soup.new_tag('h1')
+        hdr.append(title)
         new_soup.body.append(hdr)
 
-    source = soup.find(id='cphMiddle_cphMain_hlSource')
-    title = old_soup.find(id='cphMiddle_cphMain_lblTitle').get_text().strip()
-    hdr = new_soup.new_tag('title')
-    hdr.append(title)
-    new_soup.head.append(hdr)
+        #source
+        source = self.soup.find(id='cphMiddle_cphMain_hlSource')
+        if source:
+            new_soup.body.append(source)
 
-    hdr = new_soup.new_tag('h1')
-    hdr.append(title)
-    new_soup.body.append(hdr)
-    if source:
-        new_soup.body.append(source)
-    hdr = new_soup.new_tag('h3')
-    hdr.append('Ingredients')
-    new_soup.body.append(hdr)
+        #ingredients
+        hdr = new_soup.new_tag('h3')
+        hdr.append('Ingredients')
+        new_soup.body.append(hdr)
+    
+        item = self.soup.find('ul', {'class':'inggroups'})
+        if item:
+            new_soup.body.append(item)
+        else:
+            new_soup.body.append('No ingedients listed')
 
-    item = old_soup.find('ul', {'class':'inggroups'})
-    if item:
-        new_soup.body.append(item)
-    else:
-        new_soup.body.append('No ingedients listed')
+        #instructions 
+        hdr = new_soup.new_tag('h3')
+        hdr.append('Instructions')
+        new_soup.body.append(hdr)
+    
+        item = self.soup.find('ol', {'class':'dirgroupitems'})
+        if item:
+            new_soup.body.append(item)
+        else:
+            new_soup.body.append('No instructions listed')
 
-    hdr = new_soup.new_tag('h3')
-    hdr.append('Instructions')
-    new_soup.body.append(hdr)
+        #Notes 
+        hdr = new_soup.new_tag('h3')
+        hdr.append('Notes')
+        new_soup.body.append(hdr)
+    
+        notes = self.soup.find(id="cphMiddle_cphMain_lblNotes")
+        if notes:
+            hdr = new_soup.new_tag('pre')
+            hdr.append(notes.get_text())
+            new_soup.append(hdr)
+    
+        return new_soup.prettify('latin-1')
+    
+    def get_thumbnail(self):
+        tmp = self.soup.find(id='cphMiddle_cphMain_imgRecipeThumb')
+        if tmp:
+            return tmp['src']
+        else:
+            return None
 
-    item = old_soup.find('ol', {'class':'dirgroupitems'})
-    if item:
-        new_soup.body.append(item)
-    else:
-        new_soup.body.append('No instructions listed')
+class pepperplate:
 
-    hdr = new_soup.new_tag('h3')
-    hdr.append('Notes')
-    new_soup.body.append(hdr)
+    def __init__(self, hostname):
+        self.hostname = hostname
+        self.last_page = False
+        self.session = requests.Session()
 
-    notes = old_soup.find(id="cphMiddle_cphMain_lblNotes")
-    if notes:
-        hdr = new_soup.new_tag('pre')
-        hdr.append(notes.get_text())
-        new_soup.append(hdr)
+    def set_username(self, username):
+        self.username = username
 
-    return new_soup
+    def set_password(self, password):
+        self.password = password
 
-def save_recipe(title, id, soup, savepath):
-    title = title.replace('/','_').replace('"', '').replace(':','')
-    with open(savepath + '/{}.{}.html'.format(title, id), 'wb') as f:
-        f.write(soup.prettify('latin-1'))
+    def login(self):
+        if self.username == None or self.password == None:
+            print('No login details supplied')
+            return False
+
+        url = 'https://{}/login.aspx'.format(self.hostname)
+        headers = {"User-Agent":"Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2062.120 Safari/537.36"}
+
+        self.session.headers.update(headers)
+        r = s.get(url)
+
+        soup = BeautifulSoup(r.content)
+
+        VIEWSTATE = soup.find(id='__VIEWSTATE')['value']
+        EVENTVALIDATION = soup.find(id='__EVENTVALIDATION')['value']
+    
+        login_data={"__VIEWSTATE":VIEWSTATE,
+        "__EVENTVALIDATION":EVENTVALIDATION,
+        "__EVENTARGUMENT":'',
+        "__EVENTTARGET":'ctl00$cphMain$loginForm$ibSubmit',
+        "ctl00$cphMain$loginForm$tbEmail":self.username,
+        "ctl00$cphMain$loginForm$tbPassword":self.password,
+        "ctl00$cphMain$loginForm$cbRememberMe":'on'
+        }
+
+        r = self.session.post(url, data=login_data)
+        if r.url != 'http://{}/recipes/default.aspx'.format(self.hostname):
+            print('Login failure')
+            return False
+
+        return True
+
+
+    def get_page(self, page):
+
+        url = 'http://{}/recipes/default.aspx/GetPageOfResults'.format(self.hostname)
+        parameters = json.dumps({'pageIndex':page,
+                                 'pageSize':20,
+                                 'sort':4,
+                                 'tagIds': [],
+                                 'favoritesOnly':0})
+
+        headers={'Referer':'http://{}/recipes/default.aspx'.format(self.hostname)
+                         ,'Content-Type': 'application/json'
+                         ,'X-Requested-With': 'XMLHttpRequest'
+                         ,'DNT':1
+                         ,'Accept': 'application/json, text/javascript, */*; q=0.01'
+                         ,'Accept-Language': 'en,de;q=0.7,en-US;q=0.3'
+                         ,'Accept-Encoding': 'gzip, deflate'}
+        r = self.session.request('POST', url, data=parameters, headers=headers)
+
+        soup = BeautifulSoup(r.json()['d'])
+        self.page = [re.findall(r'id=(\d+)', a['href'])[0] for d in soup.find_all('div',{'class':'item'}) for a in d.find_all('a')]
+        self.last_page = len(self.page) < 20 
+
+        return self.page
+
+    def get_recipe(self, id):
+        url = 'http://{}/recipes/view.aspx?id={}'.format(self.hostname, id)
+        r = self.session.request('GET', url)
+        return r.content
+
+    def get_url(self, url):
+        r = requests.get(url)
+        return r.content
+
+    def is_last_page(self):
+        return self.last_page
+
+    def is_logged_in(self):
+        return self.session != None
+
+def save_recipe(recipe, savepath):
+    filename = recipe.get_title().replace('/','_')
+                                 .replace('"', '')
+                                 .replace(':','')
+                                 .replace(' ','_')
+    with open(savepath + '/{}.{}.html'.format(filename, recipe.get_id()), 'wb') as f:
+        f.write(recipe.get_new_body())
+
+def save_file(img, savepath):
+    with open(savepath, 'wb') as f:
+        f.write(img)
 
 if __name__ == '__main__':
     import argparse
@@ -162,21 +195,31 @@ if __name__ == '__main__':
     parser.add_argument('directory', nargs="?", default='recipes', help='Directory to which download everything. defaults to "recipes"')
     args = parser.parse_args()
 
+    if not args.password:
+        args.password = getpass.getpass('Please enter the password for account {}: '.format(args.username))
+
     imgpath = os.path.join(args.directory, 'img', '{}')
     if not os.path.exists(imgpath.format("")):
         os.makedirs(imgpath, exist_ok = True)
 
-    session = pp_login(args.username, args.password)
-    page = 0
-    soup = pp_get_page(session,page)
-    ids = scrape_recipe_ids(soup)
+    pp = pepperplate('www.pepperplate.com')
+    pp.set_username(args.username)
+    pp.set_password(args.password)
 
-    while len(ids) > 0:
-        for id in ids:
+    if not pp.login():
+        exit(1)
+
+    page = 0
+
+    while not pp.is_last_page():
+        print('Downloading page {}'.format(page+1))
+        for id in pp.get_page(page):
             time.sleep(1) #sleep 1 second between requests to not mash the server
-            title, soup = get_recipe(session, id, imgpath)
-            soup = format_recipe(soup)
-            save_recipe(title, id, soup, args.directory)
+            recipe = pepperplate_recipe(id, pp.get_recipe(id))
+            print('Downloaded {}'.format(recipe.get_title()))
+            save_recipe(id, recipe, args.directory)
+
+            if recipe.get_thumbnail():
+                save_file(pp.get_url(recipe.get_thumbnail()), imgpath.format(id + '.jpg'))
+
         page += 1
-        soup = pp_get_page(session,page)
-        ids = scrape_recipe_ids(soup)
